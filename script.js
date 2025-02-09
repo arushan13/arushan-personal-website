0f788d2e72f77850e114953d715399073c333d96
Arushan Arulraj <arushanarul13@gmail.com>
2025-02-09 14:24:17 -0500

    Updated

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMenu(){
 var slideIndex = 0;
 carousel();
 
-function carousel() {
+/*function carousel() {
   var i;
   var x = document.getElementsByClassName("about-pic");
   for (i = 0; i < x.length; i++) {
@@ -18,4 +18,4 @@ function carousel() {
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 3000); // Change image every 2 seconds
-}
+}*/
